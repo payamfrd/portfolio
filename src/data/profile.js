68292f005d12efa393aaf1661e695eeceda047ab
Payam <fard.mohammadmehdi@gmail.com>
2026-06-23 17:20:11 +0330

    Add profile file to data folder

---
+++ src/data/profile.js
@@ -0,0 +1,13 @@
+const profile = {
+  name: "Mohammadmehdi Fard",
+  title: "Front-End Develooper",
+  email: "Fard.Mohammadmehdi@Gamil.com",
+  phone: "+989301801747",
+  github: "https://github.com/payamfrd",
+  linkedin: "",
+  whatsapp: "989301801747",
+  location: "Iran/Shiraz-Asaluyeh",
+  resume: "/Fard.Mohammadmehdi.pdf",
+};
+
+export default profile;
