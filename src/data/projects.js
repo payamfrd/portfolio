591acd464eb729861fea58c91fc0b68469eb0f0f
Payam <fard.mohammadmehdi@gmail.com>
2026-06-23 17:25:55 +0330

    Add projects file to data folder

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+  {
+    id: 1,
+    title: "Portfolio Website",
+    description: "Personal Portfolio Website",
+    image: "/images/project.jpg",
+    technologies: ["Html/Css", "Javascript", "React", "Next", "Tailwind"],
+    github: "#",
+  },
+];
+
+export default projects;
